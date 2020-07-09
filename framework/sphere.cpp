a2791da35452d40da78af225a990bd0ec222a62c
neyustudies <science-cat@gmx.net>
2020-07-09 16:57:57 +0200

    task 5.2 implement methods in sphere.cpp
    - method center()
    - method radius()

--- framework/sphere.cpp
+++ framework/sphere.cpp
@@ -7,3 +7,19 @@ Sphere::Sphere() :
 Sphere::Sphere(glm::vec3 const& ctr, float r) :
   center_ {ctr},
   radius_ {r} {}
+
+glm::vec3 Sphere::center() const {
+  return center_;
+}
+
+void Sphere::center(glm::vec3 const& ctr) {
+  center_ = ctr;
+}
+
+float Sphere::radius() const {
+  return radius_;
+}
+
+void Sphere::radius(float r) {
+  radius_ = r;
+}
