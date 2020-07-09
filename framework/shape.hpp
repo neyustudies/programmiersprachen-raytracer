a06734cb90c5d4d5986de0d3d237159f3997edf2
neyustudies <science-cat@gmx.net>
2020-07-09 20:32:34 +0200

    task 5.3 add name and color to shape.hpp

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -1,14 +1,24 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
+#include "color.hpp"
+#include <string>
 
 class Shape {
 
   public:
     Shape();
-
+    Shape(std::string const& name);
+    Shape(std::string const& name, Color const& clr); 
+    std::string name() const;
+    void name(std::string const& name);
+    Color color() const;
 
     virtual float area() const = 0;
     virtual float volume() const = 0;
+
+  protected:
+    std::string name_;
+    Color color_;
 
 };
 
