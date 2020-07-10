52914d5fbfb21d7ebb0cc4a9cb8e1137bbbfea02
neyustudies <science-cat@gmx.net>
2020-07-10 13:06:34 +0200

    create hitpoint.hpp

---
+++ framework/hitpoint.hpp
@@ -0,0 +1,18 @@
+#ifndef HITPOINT_HPP
+#define HITPOINT_HPP
+#include "color.hpp"
+#include <glm/vec3.hpp>
+#include <string>
+
+struct HitPoint {
+
+  bool isIntersected  = false;
+  float t             = 0.0f;
+  std::string name    = "no name found";
+  Color clr           = {-1,-1,-1};
+  glm::vec3 point     = {-1,-1,-1};
+  glm::vec3 direction = {-1,-1,-1};
+
+};
+
+#endif // HITPOINT_HPP
