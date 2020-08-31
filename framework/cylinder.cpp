f1fbade3bdccbb975ec2c15297c74115e3443d39
neyustudies <science-cat@gmx.net>
2020-08-31 11:14:10 +0200

    cylinder: correcting members and methods
    - intersection method not fixed by now

--- framework/cylinder.cpp
+++ framework/cylinder.cpp
@@ -1,38 +1,46 @@
 #include "cylinder.hpp"
 
 Cylinder::Cylinder() :
-  Shape     {"Unnamed Cylinder", nullptr},
-  point_1_  {glm::vec3{}},
-  point_2_  {glm::vec3{}},
-  radius_   {0.0f} {}
+  Shape   {"Unnamed Cylinder", nullptr},
+  v0_     {glm::vec3{}},
+  radius_ {0.0f},
+  height_ {0.0f} {}
 
-Cylinder::Cylinder(glm::vec3 const& point_1, glm::vec3 const& point_2, float r) :
-  Shape     {"Unnamed Cylinder", nullptr},
-  point_1_  {point_1},
-  point_2_  {point_2},
-  radius_   {r} {}
+Cylinder::Cylinder(glm::vec3 const& v0, float r, float h) :
+  Shape   {"Unnamed Cylinder", nullptr},
+  v0_     {v0},
+  radius_ {r},
+  height_ {h} {}
 
-Cylinder::Cylinder(glm::vec3 const& point_1, glm::vec3 const& point_2, float r, std::string const& name, std::shared_ptr<Material> const& material) :
-  Shape     {name, material},
-  point_1_  {point_1},
-  point_2_  {point_2},
-  radius_   {r} {}
+Cylinder::Cylinder(glm::vec3 const& v0, float r, float h, std::string const& name, 
+                   std::shared_ptr<Material> const& material) :
+  Shape   {name, material},
+  v0_     {v0},
+  radius_ {r},
+  height_ {h} {}
 
 Cylinder::~Cylinder() {}
 
 float Cylinder::height() const {
-  return glm::distance(point_1_, point_2_);
+  return height_;
+}
+
+float Cylinder::radius() const {
+  return radius_;
 }
 
 float Cylinder::volume() const {
-  return powf(radius_, 2.0f) * M_PI * height();
+  return powf(radius_, 2.0f) * M_PI * height_;
 }
 
 float Cylinder::area() const {
-  return powf(radius_, 2.0f) * M_PI * 2 + radius_ * M_PI * 2 * height();
+  return powf(radius_, 2.0f) * M_PI * 2 + radius_ * M_PI * 2 * height_;
 }
 
 HitPoint Cylinder::intersect(Ray const& ray) const {
   // TODO
+  Ray tray = transformRay(world_transform_inv_, ray);
+  auto tray_direction = glm::normalize(tray.direction);
+  float distance = NAN;
   return HitPoint{};
 }
--- framework/cylinder.hpp
+++ framework/cylinder.hpp
@@ -9,18 +9,20 @@ class Cylinder : public Shape {
 
   public:
     Cylinder();
-    Cylinder(glm::vec3 const& point_1, glm::vec3 const& point_2, float r);
-    Cylinder(glm::vec3 const& point_1, glm::vec3 const& point_2, float r, std::string const& name, std::shared_ptr<Material> const& material);
+    Cylinder(glm::vec3 const& v0, float r, float h);
+    Cylinder(glm::vec3 const& v0, float r, float h, std::string const& name, 
+             std::shared_ptr<Material> const& material);
     ~Cylinder();
     float height() const;
+    float radius() const;
     float volume() const override;
     float area() const override;
     HitPoint intersect(Ray const& ray) const override;
 
   private:
-    glm::vec3 point_1_;
-    glm::vec3 point_2_;
+    glm::vec3 v0_;
     float radius_;
+    float height_;
 };
 
 #endif // CYLINDER_HPP
