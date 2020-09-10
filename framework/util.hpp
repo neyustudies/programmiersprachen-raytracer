218ce0f42722bb7985c360ddaf5f010d7ed2639d
Jakob Ruckel <jakob@ruckel.de>
2020-09-10 10:30:36 +0200

    util: add operator<< for vec3

--- framework/util.hpp
+++ framework/util.hpp
@@ -1,11 +1,18 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
+#include <ostream>
+#include <glm/vec3.hpp>
+
 #define EPSILON 0.0001f
 
 template <typename T>
 inline bool in_between_epsilon(T min, T f, T max) {
   return (min - EPSILON) <= (f + EPSILON) && (f - EPSILON) <= (max + EPSILON);
+}
+
+inline std::ostream& operator<<(std::ostream& os, glm::vec3 vec) {
+  return os << vec.x << ", " << vec.y << ", " << vec.z;
 }
 
 #endif  // UTIL_HPP
